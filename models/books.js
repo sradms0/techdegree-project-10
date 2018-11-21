1a3142e47897c67fc10943542c3c1b3b7406a71e
sradms0 <sradms0@openmailbox.org>
2018-11-20 21:23:22 -0500

    initial commit

---
+++ models/books.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const books = sequelize.define('books', {
+    title: DataTypes.STRING,
+    author: DataTypes.STRING,
+    genre: DataTypes.STRING,
+    first_published: DataTypes.INTEGER
+  }, {});
+  books.associate = function(models) {
+    // associations can be defined here
+  };
+  return books;
+};
