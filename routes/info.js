a3e18bfdde6a276e73182bdf4b765375e276bd1f
sradms0 <sradms0@openmailbox.org>
2018-11-23 17:28:06 -0500

    implement queryBuilder, and rowParser, and pass modified data to render

--- routes/info.js
+++ routes/info.js
@@ -26,7 +26,11 @@ router.get(/all|overdue|checked|new/, (req, res) => {
     const title = setTitle(subject, type);
     const tableHeaders = setTableHeaders(subject);
 
-    res.render('page', {title, subject, type, tableHeaders});
+    queryBuilder(models, subject, type)
+        .then(data => {
+            const rows = rowsParser(data, subject);
+            res.render('page', {title, subject, type, tableHeaders, rows});
+        })
 });
 
 module.exports = router;
