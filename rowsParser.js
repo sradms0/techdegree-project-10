c63888e868509bd9cee281f76d9cdc118693def2
sradms0 <sradms0@openmailbox.org>
2018-11-23 17:29:34 -0500

    fix index slicing to include patron's address

--- rowsParser.js
+++ rowsParser.js
@@ -6,7 +6,7 @@ module.exports = (data, subject) => {
     const joinName = n => [ values(n)[0]+' '+values(n)[1] ];
     const parse = {
         'books':    () => data.map(i => values(i)),
-        'patrons':  () => data.map(i => [...joinName(i), ...values(i).slice(3) ]),
+        'patrons':  () => data.map(i => [...joinName(i), ...values(i).slice(2) ]),
         'loans':    () => (
                             data.map(i => {
                                 const dataValues = values(i);
