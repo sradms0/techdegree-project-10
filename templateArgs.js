1fbde63d585b5e0de8603c37f6111eec4588a946
sradms0 <sradms0@openmailbox.org>
2018-11-24 22:56:43 -0500

    refactor to account for improved templatePrep utilities

--- templateArgs.js
+++ templateArgs.js
@@ -1,14 +1,22 @@
 'use strict';
 
-const {setTableHeaders, setTitle, sliceUrl, setItem} = require('./templatePrep');
+const utils = require('./templatePrep');
 
-module.exports = (req, res) => {
+module.exports = (req, res, config) => {
+    const {setTitle, sliceUrl, setItem} = utils.general;
+    const {setTableHeaders} = utils.table;
+    const {setFormLabels} = utils.form;
+
     const subject = sliceUrl(req.baseUrl);
     const type = sliceUrl(req.url);
     const title = setTitle(subject, type);
     const item = setItem(subject);
-    const tableHeaders = setTableHeaders(subject);
 
-    return ({subject,type,title,item,tableHeaders});
+    const args = {subject,type,title,item};
+
+    if (config === 'table') args.tableHeaders = setTableHeaders(subject);
+    if (config === 'form')  args.formLabels   = setFormLabels(subject);
+
+    return args;
 }
 
