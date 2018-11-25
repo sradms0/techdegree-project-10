2a3a940328274f827b3a13804ca23006f54fdaf9
sradms0 <sradms0@openmailbox.org>
2018-11-24 22:58:24 -0500

    add functionality for forms and refactor for imporoved modularity

--- templatePrep.js
+++ templatePrep.js
@@ -1,70 +1,114 @@
 'use strict';
 
-module.exports = {
-    // for effectively retrieving the subject&type 
-    //  from the url: eliminate '/'
-    sliceUrl: url => url.slice(1),
+module.exports = ( () => {
+    return ({
+        general: {
+            // for effectively retrieving the subject&type 
+            //  from the url: eliminate '/'
+            sliceUrl: url => url.slice(1),
 
-    // set headers for table depending on table type
-    setTableHeaders: subject => {
-        let tableHeaders = [];
-        switch (subject) {
-            case 'books':
-                tableHeaders = [
-                    'Title',
-                    'Author',
-                    'Genre',
-                    'Year Released'
-                ];
-                break;
-            case 'patrons':
-                tableHeaders = [
-                    'Name',
-                    'Address',
-                    'Email',
-                    'Library ID',
-                    'Zip'
-                ];
-                break;
-            case 'loans':
-                tableHeaders = [
-                    'Book',
-                    'Patron',
-                    'Loaned on',
-                    'Return by', 	
-                    'Returned on', 	
-                    'Action'
-                ];
-                break;
-            default:
-                break;
-        }
-        return tableHeaders;
-    },
+            // for create button 
+            setItem: subject => subject[0].toUpperCase()+subject.slice(1,-1),
 
-    // create title for template header based on type
-    setTitle: (subject, type) => {
-        let title = '';
-        switch (type) {
-            case 'overdue':
-                title = 'Overdue ';
-                break;
-            case 'checked':
-                title = 'Checked Out ';
-                break;
-            case 'new':
-                title = 'New ';
-                break;
-            default:
-                break;
+            // create title for template header based on type
+            setTitle: (subject, type) => {
+                let title = '';
+                switch (type) {
+                    case 'overdue':
+                        title = 'Overdue ';
+                        break;
+                    case 'checked':
+                        title = 'Checked Out ';
+                        break;
+                    case 'new':
+                        title = 'New ';
+                        break;
+                    default:
+                        break;
+                }
+                // change to subject to books for titling
+                if (subject === 'loans' && type === 'checked') subject = 'books';
+                title += subject[0].toUpperCase()+subject.slice(1);
+                // if creating a new item, slice off 's' in title
+                if (type === 'new') title = title.slice(0, -1);
+                return title;
+            }
+        },
+
+        form: {
+            setFormLabels: subject => {
+                let formLabels = [];
+                switch (subject) {
+                    case 'books':
+                        formLabels = [
+                            'Title',
+                            'Author',
+                            'Genre',
+                            'First Published'
+                        ];
+                        break;
+                    case 'patrons':
+                        formLabels = [
+                            'First Name',
+                            'Last Name',
+                            'Address',
+                            'Email',
+                            'Library ID',
+                            'Zip Code'
+                        ];
+                        break;
+                    case 'loans':
+                        formLabels = [
+                            'Book',
+                            'Patron',
+                            'Loaned on:',
+                            'Return by:'
+                        ];
+                        break;
+                    default:
+                        break;
+                }
+                return formLabels.map(i => ({text: i, forID: i.toLowerCase().replace(' ', '_')}));
+            }
+        },
+
+        table: {
+            // set headers for table depending on table type
+            setTableHeaders: subject => {
+                let tableHeaders = [];
+                switch (subject) {
+                    case 'books':
+                        tableHeaders = [
+                            'Title',
+                            'Author',
+                            'Genre',
+                            'Year Released'
+                        ];
+                        break;
+                    case 'patrons':
+                        tableHeaders = [
+                            'Name',
+                            'Address',
+                            'Email',
+                            'Library ID',
+                            'Zip'
+                        ];
+                        break;
+                    case 'loans':
+                        tableHeaders = [
+                            'Book',
+                            'Patron',
+                            'Loaned on',
+                            'Return by', 	
+                            'Returned on', 	
+                            'Action'
+                        ];
+                        break;
+                    default:
+                        break;
+                }
+                return tableHeaders;
+            }
         }
-        // change to subject to books for titling
-        if (subject === 'loans' && type === 'checked') subject = 'books';
-        title += subject[0].toUpperCase()+subject.slice(1);
-        // if creating a new item, slice off 's' in title
-        if (type === 'new') title = title.slice(0, -1);
-        return title;
-    },
-    // for create button 
-    setItem: subject => subject[0].toUpperCase()+subject.slice(1,-1)
-};
+    });
+})();
